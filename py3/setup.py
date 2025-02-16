f4cbd61c35048447c3ec9dfd3754e41b24664afe
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-15 23:41:21 -0800

    0.26 unisos and blee elimination.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.26 -- forSysVersion=0.2 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.27 -- forLocalVersion=0.2 -- constant=NA
 def pkgVersion():
         return '0.2'
 
@@ -53,9 +53,6 @@ def pkgVersion():
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
 requires = [
-"blee",
-"bisos.csPlayer",
-"blee.csPlayer",
 "bisos",
 "bisos.b",
 "bisos.common",
@@ -68,9 +65,7 @@ requires = [
 
 scripts = [
 'bin/basics-fileLine.cs',
-'bin/basics-fileLine.cs.20250210130125088361082',
 'bin/pyLiteralTo.cs',
-'bin/pyLiteralTo.cs.20250210130125099832920',
 ]
 ####+END:
 
